b362b5b1ef8279aec8abc57ade450137ab5caa01
Vladyslav Huba <vladhuba@gmail.com>
2023-05-01 16:53:47 +0300

    speed things up

--- src/components/App/propagator-worker.ts
+++ src/components/App/propagator-worker.ts
@@ -10,6 +10,20 @@ function lookAnglesToCartesian (elevation: number, azimuth: number): [number, nu
   return [y, z, x]
 }
 
+function concat (arrays: Float32Array[]) {
+  const totalLength = arrays.reduce((acc, value) => acc + value.length, 0)
+
+  const result = new Float32Array(totalLength)
+
+  let length = 0
+  for (const array of arrays) {
+    result.set(array, length)
+    length += array.length
+  }
+
+  return result
+}
+
 type ParametersExceptFirst<F> =
    F extends (arg0: any, ...rest: infer R) => any ? R : never;
 
@@ -20,15 +34,36 @@ function typedPostMessage (message: WorkerAnswer, ...rest: ParametersExceptFirst
 let satRecs: satellite.SatRec[] = []
 
 const geometriesMap = new Map<string, MsdfGeometry>()
+const geometriesPositionsMap = new Map<string, Float32Array>()
+const geometriesUVCoordsMap = new Map<string, Float32Array>()
 
 self.onmessage = (e: MessageEvent<WorkerQuery>) => {
   if (e.data.type === 'init') {
     geometriesMap.clear()
+    geometriesPositionsMap.clear()
+    geometriesUVCoordsMap.clear()
     const geometryBuilder = new MsdfGeometryBuilder(e.data.msdfDefinition)
     satRecs = e.data['3LEs']
       .map(tle => {
         const satRec = satellite.twoline2satrec(tle[1], tle[2])
-        geometriesMap.set(satRec.satnum, geometryBuilder.textToGeometry(tle[0].slice(2)))
+        const geometry = geometryBuilder.textToGeometry(tle[0].slice(2))
+        geometriesMap.set(satRec.satnum, geometry)
+        geometriesPositionsMap.set(satRec.satnum, new Float32Array(geometry.chars.flatMap(char => [
+          char.iXY[0], char.iXY[1],
+          char.iXY[0] + char.iSize[0], char.iXY[1],
+          char.iXY[0] + char.iSize[0], char.iXY[1] + char.iSize[1],
+          char.iXY[0], char.iXY[1],
+          char.iXY[0], char.iXY[1] + char.iSize[1],
+          char.iXY[0] + char.iSize[0], char.iXY[1] + char.iSize[1]
+        ])))
+        geometriesUVCoordsMap.set(satRec.satnum, new Float32Array(geometry.chars.flatMap(char => [
+          char.iUV[0], char.iUV[1],
+          char.iUV[0] + char.iSize[0], char.iUV[1],
+          char.iUV[0] + char.iSize[0], char.iUV[1] + char.iSize[1],
+          char.iUV[0], char.iUV[1],
+          char.iUV[0], char.iUV[1] + char.iSize[1],
+          char.iUV[0] + char.iSize[0], char.iUV[1] + char.iSize[1]
+        ])))
         return satRec
       })
       .filter(satRec => satRec.error === 0)
@@ -65,23 +100,9 @@ self.onmessage = (e: MessageEvent<WorkerQuery>) => {
     const successful = (positions.filter((position) => position.cartesian) as {norad: string, cartesian: [number, number, number]}[]).filter(position => position.cartesian[1] > 0)
     const positionsArray = new Float32Array(successful.flatMap(position => position.cartesian!))
 
-    const textsOrigins = new Float32Array(successful.flatMap(satellite => geometriesMap.get(satellite.norad)!.chars.flatMap(() => Array.from({ length: 6 }, () => satellite.cartesian).flat())))
-    const textsPositions = new Float32Array(successful.flatMap(satellite => geometriesMap.get(satellite.norad)!.chars.flatMap(char => [
-      char.iXY[0], char.iXY[1],
-      char.iXY[0] + char.iSize[0], char.iXY[1],
-      char.iXY[0] + char.iSize[0], char.iXY[1] + char.iSize[1],
-      char.iXY[0], char.iXY[1],
-      char.iXY[0], char.iXY[1] + char.iSize[1],
-      char.iXY[0] + char.iSize[0], char.iXY[1] + char.iSize[1]
-    ])))
-    const textsUVCoords = new Float32Array(successful.flatMap(satellite => geometriesMap.get(satellite.norad)!.chars.flatMap(char => [
-      char.iUV[0], char.iUV[1],
-      char.iUV[0] + char.iSize[0], char.iUV[1],
-      char.iUV[0] + char.iSize[0], char.iUV[1] + char.iSize[1],
-      char.iUV[0], char.iUV[1],
-      char.iUV[0], char.iUV[1] + char.iSize[1],
-      char.iUV[0] + char.iSize[0], char.iUV[1] + char.iSize[1]
-    ])))
+    const textsOrigins = concat(successful.map(satellite => Float32Array.from({ length: 3 * 6 * geometriesMap.get(satellite.norad)!.chars.length }, (v, i) => satellite.cartesian[i % 3]!)))
+    const textsPositions = concat(successful.map(satellite => geometriesPositionsMap.get(satellite.norad)!))
+    const textsUVCoords = concat(successful.map(satellite => geometriesUVCoordsMap.get(satellite.norad)!))
     // eslint-disable-next-line no-debugger
 
     typedPostMessage({
